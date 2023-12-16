eb0c5fd1f374f8b91b3f5782c72bcf5f1fe38c50
wellsy173 <62893834+wellsy173@users.noreply.github.com>
2023-12-16 13:33:23 +0800

    Add files via upload

---
+++ Mooskine/Model/DataController.swift
@@ -0,0 +1,32 @@
+//
+//  DataController.swift
+//  Mooskine
+//
+//  Created by Simon Wells on 2023/12/16.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load (completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion? ()
+        }
+    }
+}
---
+++ Mooskine/Model/Notebook + Extra.swift
@@ -0,0 +1,13 @@
+//
+//  Notebook + Extra.swift
+//  Mooskine
+//
+//  Created by Simon Wells on 2023/12/15.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import Foundation
+
+extension Notebook {
+    
+}
